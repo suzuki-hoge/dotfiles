655fa21d862add6f1eb70064325e7f3376801f50
suzuki-hoge <user.ryo@gmail.com>
2014-04-20 01:07:19 +0900

    remove option -n from gst

--- git/util.py
+++ git/util.py
@@ -1,10 +1,16 @@
 import subprocess
 
 def getStdout(command):
-	return subprocess.Popen(command.split(), stdout = subprocess.PIPE).stdout.read().strip()
+	return subprocess.Popen(command.split(), stdout = subprocess.PIPE).stdout
+
+def getStdoutString(command):
+	return getStdout(command).read().strip()
+
+def getStdoutList(command):
+	return getStdout(command)
 
 def getCurrentBranch():
-	return getStdout('git rev-parse --abbrev-ref HEAD')
+	return getStdoutString('git rev-parse --abbrev-ref HEAD')
 
 def getRootDir():
-	return getStdout('git rev-parse --show-toplevel')
+	return getStdoutString('git rev-parse --show-toplevel')
