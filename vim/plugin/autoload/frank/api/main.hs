2a465126ac778bbd0f3771f2227b87a9f744b027
suzuki-hoge <user.ryo@gmail.com>
2015-05-06 13:08:00 +0900

    h-frank : indent format

--- vim/plugin/autoload/frank/api/main.hs
+++ vim/plugin/autoload/frank/api/main.hs
@@ -1,20 +1,29 @@
 import Entry
 import Output
 import System.Environment(getArgs)
-
-output mode root | mode == "indented" = Output.indented (kind root) root
-output mode root | mode == "full"     = Output.full (kind root) root
-output _    _                         = putStrLn "invalid arguments"
+import System.Directory
+import Data.List.Split
+import System.FilePath.Posix
 
 main = do
---    args <- getArgs
---    let path = args !! 0
---    let mode = args !! 1
+    args <- getArgs
+    let path = args !! 0
+    let mode = args !! 1
 
-    let path = "/Users/ryo/Development/Haskell-Practice"
-    let mode = "indented"
+    if mode == "indented"
+        then do
+            let root = dropFileName path
+            let name = takeFileName path
 
-    subs <- Entry.getEntries path
-    let root = Entry.Directory { path = path, kind = Entry.D, subs = subs }
+            setCurrentDirectory root
 
-    output mode root
+            subs <- Entry.getEntries name
+            let rootEntry = Entry.Directory { path = name, kind = Entry.D, subs = subs }
+
+            Output.indented (kind rootEntry) rootEntry
+
+        else do
+            subs <- Entry.getEntries path
+            let rootEntry = Entry.Directory { path = path, kind = Entry.D, subs = subs }
+
+            Output.full (kind rootEntry) rootEntry
