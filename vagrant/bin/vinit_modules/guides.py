6f5c6e9fa3b951ebc63faa28f115dbe736ff0260
suzuki-hoge <user.ryo@gmail.com>
2015-05-19 16:05:15 +0900

    init : refactoring and modify template

--- vagrant/bin/vinit_modules/guides.py
+++ vagrant/bin/vinit_modules/guides.py
@@ -6,52 +6,56 @@ os.system("stty erase '^?'")
 
 def name_guide():
     inputted = raw_input('\nname:  ')
-    if inputted != '':
-	return inputted
-    else:
-	name_guide()
+    return inputted if inputted else name_guide()
 
 
 def box_guide():
-    boxes = filter(lambda name: name != '.DS_Store', os.listdir('%s/.vagrant.d/boxes/' % os.environ['HOME']))
+    def _box_name_guide():
+        inputted = raw_input('\nbox name:  ')
+        return inputted if inputted else _box_name_guide()
+
+    def _box_url_guide():
+        inputted = raw_input('\nbox url:  ')
+        return inputted if inputted else _box_url_guide()
+
+    boxes = [box for box in os.listdir('%s/.vagrant.d/boxes' % os.environ['HOME']) if box != '.DS_Store']
 
     print '\n',
     for i, box in enumerate(boxes):
-	print '%d %s' % (i, box)
+        print '%d %s' % (i, box)
 
-    inputted = raw_input('\nbox (leave blank to use new box and box_url):  ')
+    inputted = raw_input('\nbox (leave blank to input new box and box_url):  ')
 
     if inputted == '':
-	box_name = raw_input('\nbox name:  ')
-	box_url = raw_input('\nbox url:   ')
+        return _box_name_guide(), _box_url_guide()
     else:
-	try:
-	    box_name = boxes[int(inputted)]
-	    box_url = ''
-	except IndexError:
-	    box_name, box_url = box_guide()
+        try:
+            box_name = boxes[int(inputted)]
+            box_url = ''
+        except (IndexError, ValueError):
+            box_name, box_url = box_guide()
 
-    return box_name, box_url
+        return box_name, box_url
 
 
 def forwarding_guide(guests = [], hosts = []):
     def _forwarding_guest_guide():
-	inputted = raw_input('\nguest forwarded port (leave blank to go next configure): ')
-	return inputted
+        inputted = raw_input('\nguest forwarded port (leave blank to go next configure): ')
+        return inputted
 
     def _forwarding_host_guide():
-	inputted = raw_input('\nhost forwarded port: ')
-	return inputted if inputted != '' else _forwarding_host_guide()
+        inputted = raw_input('\nhost forwarded port: ')
+        return inputted if inputted else _forwarding_host_guide()
 
     inputed_guest = _forwarding_guest_guide()
 
     if inputed_guest == '':
-	return zip(guests, hosts)
+        return zip(guests, hosts)
     else:
-	inputed_host = _forwarding_host_guide()
-	guests.append(inputed_guest)
-	hosts.append(inputed_host)
-	return forwarding_guide(guests, hosts)
+        inputed_host = _forwarding_host_guide()
+        guests.append(inputed_guest)
+        hosts.append(inputed_host)
+        return forwarding_guide(guests, hosts)
 
 
 def private_ip_guide():
@@ -68,17 +72,18 @@ def spec_guide():
     inputted = raw_input('\nspec customize? y/n: ')
 
     if inputted == 'y':
-	def memory_guide():
-	    inputted = raw_input('\nmemory (MB): ')
-	    return inputted if inputted != '' else memory_guide()
+        def memory_guide():
+            inputted = raw_input('\nmemory (MB): ')
+            return inputted if inputted else memory_guide()
 
-	def cpus_guide():
-	    inputted = raw_input('\ncpus: ')
-	    return inputted if inputted != '' else cpus_guide()
+        def cpus_guide():
+            inputted = raw_input('\ncpus: ')
+            return inputted if inputted else cpus_guide()
 
-	spec = {'memory' : memory_guide(), 'cpus' : cpus_guide()}
-	return spec
-    if inputted == 'n':
-	return {}
+        spec = {'memory' : memory_guide(), 'cpus' : cpus_guide()}
+        return spec
+
+    elif inputted == 'n':
+        return {}
     else:
-	return spec_guide()
+        return spec_guide()
--- vagrant/bin/vinit_modules/template.py
+++ vagrant/bin/vinit_modules/template.py
@@ -1,19 +1,28 @@
 def vagrantfile(name, box_name, box_url, forwardings, private_ip, provision_path, spec):
-    name_line = "vbox.name = '%s'" % name
+    name_line = format("vbox.name = '%s'", name)
 
-    box_name_line = "\n  config.vm.box = '%s'" % box_name
-    box_url_line  = "\n  config.vm.box_url = '%s'" % box_url if box_url != '' else 'no line'
+    box_name_line = format("\n  config.vm.box = '%s'", box_name)
 
-    forwarding_lines = ''.join(["\n  config.vm.network :forwarded_port, guest: %(guest)s, host: %(host)s" % locals() for guest, host in forwardings]) if forwardings != [] else 'no line'
+    box_url_line  = format_or("\n  config.vm.box_url = '%s'", box_url, '')
 
-    private_ip_line = "\n  config.vm.network :private_network, ip: '%s'" % private_ip if private_ip != '' else 'no line'
+    forwarding_lines = ''.join([format("\n  config.vm.network :forwarded_port, guest: %(guest)s, host: %(host)s", locals()) for guest, host in forwardings])
+    forwarding_lines = forwarding_lines if forwarding_lines else 'no line'
 
-    provision_path_line  = "\n  config.vm.provision :shell, :path => '%s'" % provision_path if provision_path != '' else 'no line'
+    private_ip_line = format_or("\n  config.vm.network :private_network, ip: '%s'", private_ip)
 
-    spec_line = "\n  config.vm.provider :virtualbox do |vb|\n    vb.customize ['modifyvm', :id, '--memory', '%(memory)s', '--cpus', '%(cpus)s', '--ioapic', 'on']\n  end" % spec if spec != {} else 'no line'
+    provision_path_line  = format_or("\n  config.vm.provision :shell, :path => '%s'", provision_path)
+
+    spec_line = format_or("\n  config.vm.provider :virtualbox do |vb|\n    vb.customize ['modifyvm', :id, '--memory', '%(memory)s', '--cpus', '%(cpus)s', '--ioapic', 'on']\n  end", spec)
 
     return _template(name_line, box_name_line, box_url_line, forwarding_lines, private_ip_line, provision_path_line, spec_line)
 
+
+def format(_format, _value):
+    return _format % _value
+
+
+def format_or(_format, _value, _or = 'no line'):
+    return _format % _value if _value else _or
 
 def _template(name_line, box_name_line, box_url_line, forwarding_lines, private_ip_line, provision_path_line, spec_line):
     line = """# -*- mode: ruby -*-
@@ -25,8 +34,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provider :virtualbox do |vbox|
     %(name_line)s
   end
-  %(box_name_line)s
-  %(box_url_line)s
+  %(box_name_line)s%(box_url_line)s
   %(forwarding_lines)s
   %(private_ip_line)s
   %(provision_path_line)s
