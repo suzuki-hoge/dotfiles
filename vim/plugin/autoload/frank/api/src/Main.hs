e875ca784d270ce2ca6865321fc644b241e607ae
suzuki-hoge <user.ryo@gmail.com>
2015-05-28 00:40:10 +0900

    frank : refactoring

--- vim/plugin/autoload/frank/api/src/Main.hs
+++ vim/plugin/autoload/frank/api/src/Main.hs
@@ -1,44 +1,26 @@
-import System.Process
-import Control.Applicative
 import Data.List.Split
-import Control.Monad
-import System.Directory
+
+import System
 
 
-mkOptions :: String -> [String] -> [String]
-mkOptions path blackNames = [path] ++ (blacks blackNames) ++ ["-ls"]
-
-
-blacks :: [String] -> [String]
-blacks []     = []
-blacks [x]    = black x
-blacks (x:xs) = black x ++ blacks xs
-
-
-black :: String -> [String]
-black name = ["-name", name, "-prune", "-or"]
-
-
--- find :: [String] -> Path
-find options = readProcess "find" options []
-
-
-mk root aline = indent ++ name ++ slash
-    where ftype  = splitOn " " aline !! 0
-          full   = splitOn " " aline !! 1
+mkIndentedLine :: Path -> String -> String
+mkIndentedLine root awkedLines = indent ++ name ++ slash
+    where ftype  = splitOn " " awkedLines !! 0
+          full   = splitOn " " awkedLines !! 1
           path   = drop (length root) full
           depth  = length $ splitOn "/" path
           indent = take ((depth -1) * 4) $ repeat ' '
           name   = last $ splitOn "/" full
           slash  = if ftype !! 0 == 'd' then "/" else ""
 
+
 main = do
     let root = "/Users/ryo/.dotfiles/vim/plugin/autoload/pete"
-    let options = mkOptions root [".git", ".svn", ".vagrant", ".DS_Store", "*.pyc", "dist", ".cabal-sandbox"]
+    let findOptions = mkFindOptions root [".git", ".svn", ".vagrant", ".DS_Store", "*.pyc", "dist", ".cabal-sandbox"]
     
 
-    ls <- find options
-    alines <- readProcess "awk" ["{ print $3,$11 }"] ls
+    foundLines <- find findOptions
+    awkedLines <- awk mkAwkOptions foundLines
 
-    let es = map (mk root) $ lines alines
-    putStr $ unlines es
+    let indentedLines = map (mkIndentedLine root) $ lines awkedLines
+    putStr $ unlines indentedLines
---
+++ vim/plugin/autoload/frank/api/src/System.hs
@@ -0,0 +1,40 @@
+module System(
+Path,
+find,
+mkFindOptions,
+awk,
+mkAwkOptions
+) where
+
+
+import System.Process
+
+
+type Path = String
+type Black = String
+type Options = [String]
+
+
+find :: Options -> IO String
+find options = readProcess "find" options []
+
+
+mkFindOptions :: Path -> [Black] -> Options
+mkFindOptions path blackNames = [path] ++ (blacks blackNames) ++ ["-ls"]
+
+
+awk :: Options -> String -> IO String
+awk options stdin = readProcess "awk" options stdin
+
+mkAwkOptions :: Options
+mkAwkOptions = ["{ print $3,$11 }"]
+
+
+blacks :: [Black] -> [String]
+blacks []     = []
+blacks [x]    = black x
+blacks (x:xs) = black x ++ blacks xs
+
+
+black :: Black -> [String]
+black name = ["-name", name, "-prune", "-or"]
