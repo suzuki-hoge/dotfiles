e0a9d120f24610854f5696e7afcd430539658a8d
suzuki-hoge <user.ryo@gmail.com>
2015-05-28 11:57:00 +0900

    frank : command line argumentable

--- vim/plugin/autoload/frank/api/src/Main.hs
+++ vim/plugin/autoload/frank/api/src/Main.hs
@@ -1,15 +1,23 @@
+import System.Environment(getArgs)
+import Control.Applicative
+
 import System
 import Output
 
 
 main = do
-    let root = "/Users/ryo/.dotfiles/vim/plugin/autoload/pete"
+    root <- (!! 0) <$> getArgs
+    mode <- (!! 1) <$> getArgs
+
     let findOptions = mkFindOptions root [".git", ".svn", ".vagrant", ".DS_Store", "*.pyc", "dist", ".cabal-sandbox"]
-    
 
     foundLines <- find findOptions
     awkedLines <- awk mkAwkOptions foundLines
 
     let entries = map (mkEntry root) $ lines awkedLines
-    indentOutput entries
-    fullOutput entries
+
+    if mode == "indent"
+        then
+            indentOutput entries
+        else
+            fullOutput entries
