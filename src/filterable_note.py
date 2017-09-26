63de43dc8323d8fb6bce6f49c1abfdd02a39c99e
suzuki-hoge <user.ryo@gmail.com>
2017-09-26 17:53:51 +0900

    refactoring for ui

--- src/filterable_note.py
+++ src/filterable_note.py
@@ -1,11 +1,52 @@
 from pure import converter, formatter
 from system import filer
 
-print filer.local_notes()
+print filer.notes('local')
 
-json = filer.local_note_lines('url')
+json = filer.read('local', 'url')
 masked_json = converter.mask(json)
 
-print converter.to_value('1 | foo label | foo tag 1, foo tag 2 | ***', json)
+print converter.to_value('1', json)
 
 print formatter.vertical_align(masked_json)
+
+
+"""
+fng
+
+    path
+  > url
+    password
+"""
+
+"""
+fng url
+
+    1 | foo | ...
+  > 2 | foo | ...
+    3 | foo | ...
+"""
+
+"""
+fnl
+
+    path
+  > url
+    password
+"""
+
+"""
+fnl url
+
+    1 | foo | ...
+  > 2 | foo | ...
+    3 | foo | ...
+"""
+
+"""
+python filterable_note.py global
+
+python filterable_note.py global url
+
+python filterable_note.py global url 1
+"""
