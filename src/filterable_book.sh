8949c30d3be7cb7158f547d4a34d641ed941410b
suzuki-hoge <user.ryo@gmail.com>
2017-09-28 10:33:39 +0900

    rename

---
+++ src/filterable_book.sh
@@ -0,0 +1,23 @@
+dir=$(cd $(dirname $0); pwd)
+script=${dir}/filterable_book.py
+
+book=$1
+
+
+if [ "$#" = 1 ]; then
+    note=`python $script $1 | percol`
+else
+    note=$2
+fi
+
+
+page=`python $script $book $note | percol`
+
+IFS='|'
+set -- $page
+line_id=`echo $1 | tr -d ' '`
+
+
+line_value=`python $script $book $note $line_id`
+
+echo $line_value
--- src/pure/formatter.py
+++ src/pure/formatter.py
@@ -1,16 +1,16 @@
 def vertical_align(json):
-    max_width_1 = __max_witdh(json, lambda key, element: len(key))
-    max_width_2 = __max_witdh(json, lambda key, element: len(element['label']))
-    max_width_3 = __max_witdh(json, lambda key, element: len(element['tags']))
+    max_width_1 = __max_witdh(json, lambda line_id, line: len(line_id))
+    max_width_2 = __max_witdh(json, lambda line_id, line: len(line['label']))
+    max_width_3 = __max_witdh(json, lambda line_id, line: len(line['tags']))
 
-    aligned_lines = [__to_line(key, element, max_width_1, max_width_2, max_width_3) for key, element in json.iteritems()]
+    aligned_lines = [__to_line(line_id, line, max_width_1, max_width_2, max_width_3) for line_id, line in json.iteritems()]
     aligned_lines.sort()
     return '\n'.join(aligned_lines)
 
 
 def __max_witdh(json, width):
-    return max([width(key, element) for key, element in json.iteritems()])
+    return max([width(line_id, line) for line_id, line in json.iteritems()])
 
 
-def __to_line(key, element, max_width_1, max_width_2, max_width_3):
-    return ' | '.join([key.rjust(max_width_1), element['label'].ljust(max_width_2), element['tags'].ljust(max_width_3), element['value']])
+def __to_line(line_id, line, max_width_1, max_width_2, max_width_3):
+    return ' | '.join([line_id.rjust(max_width_1), line['label'].ljust(max_width_2), line['tags'].ljust(max_width_3), line['value']])
