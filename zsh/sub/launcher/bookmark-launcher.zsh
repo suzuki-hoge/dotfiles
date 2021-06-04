2441f84ad45dc14ba4bdf2a56e5594015db2e187
suzuki-hoge <user.ryo@gmail.com>
2021-06-04 16:23:31 +0900

    add launcher args

--- zsh/sub/launcher/bookmark-launcher.zsh
+++ zsh/sub/launcher/bookmark-launcher.zsh
@@ -1,6 +1,18 @@
 function bl() {
-  key=`python ~dot/zsh/sub/launcher/bookmark-parser.py keys | percol --match-method regex | xargs echo`
-  val=`python ~dot/zsh/sub/launcher/bookmark-parser.py val "$key"`
+  if [ -n "$1" ]; then
+    explicit=`python ~dot/zsh/sub/launcher/bookmark-parser.py keys | grep $1`
+
+    if [ 1 -eq `echo $explicit | wc -l` ]; then
+      key=$explicit
+      val=`python ~dot/zsh/sub/launcher/bookmark-parser.py val "$key"`
+    else
+      key=`echo $explicit | percol --match-method regex | xargs echo`
+      val=`python ~dot/zsh/sub/launcher/bookmark-parser.py val "$key"`
+    fi
+  else
+    key=`python ~dot/zsh/sub/launcher/bookmark-parser.py keys | percol --match-method regex | xargs echo`
+    val=`python ~dot/zsh/sub/launcher/bookmark-parser.py val "$key"`
+  fi
 
   open $val
 }
